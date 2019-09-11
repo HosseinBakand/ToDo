f0b9daaf23936ad7694cf466fa237359ebfed4de
Mohammad Bahadori <mohammad.bahadori@partdp.ir>
2019-09-11 08:03:47 -0400

    edit bindingAdapter [#31] -edit picasso's call

--- app/src/main/java/i/part/app/course/todo/core/util/ui/BindingAdapter.kt
+++ app/src/main/java/i/part/app/course/todo/core/util/ui/BindingAdapter.kt
@@ -3,16 +3,24 @@ package i.part.app.course.todo.core.util.ui
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
-import i.part.app.course.todo.R
-
-@BindingAdapter("imageUrlForCircular")
-fun loadCirularImage(view: ImageView, imageUrl: String) {
-    val trsfrm = PicassoCircleTransformation()
-    Picasso.get().load(imageUrl).transform(trsfrm).error(R.drawable.ic_person_gray_24dp)
-}
 
 @BindingAdapter("imageUrlForRoundCorner")
 fun loadRoundCornerImage(view: ImageView, imageUrl: String) {
-    val trsfrm = RoundedCornersTransformation(8, 0)
-    Picasso.get().load(imageUrl).transform(trsfrm).error(R.drawable.ic_person_gray_24dp)
+    val radius = 8
+    val margin = 0
+    val transformation = RoundedCornersTransformation(radius, margin)
+    Picasso.get()
+        .load(imageUrl)
+        .transform(transformation)
+        .fit()
+        .into(view)
+}
+
+@BindingAdapter("imageUrlForCirular")
+fun loadCirularImage(view: ImageView, imageUrl: String) {
+    val transform = PicassoCircleTransformation()
+    Picasso.get()
+        .load(imageUrl)
+        .transform(transform)
+        .into(view)
 }
