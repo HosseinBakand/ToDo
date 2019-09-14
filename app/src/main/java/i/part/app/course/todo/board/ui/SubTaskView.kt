108befc30ef1cf717d23ca772273a87496ebfda7
Vahid Safari <vahid.safari@partdp.ir>
2019-09-14 03:01:38 -0400

    (#34) 9/12/2019
    todolist view contains a list of subtasks

--- app/src/main/java/i/part/app/course/todo/board/ui/SubTaskView.kt
+++ app/src/main/java/i/part/app/course/todo/board/ui/SubTaskView.kt
@@ -2,5 +2,5 @@ package i.part.app.course.todo.board.ui
 
 data class SubTaskView(
     var subTaskDescription: String,
-    var isCompleted: Boolean,
+    var isCompleted: Boolean = false,
     var imageUri: String = "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png")
