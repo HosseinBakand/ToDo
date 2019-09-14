57984bce3ef5dea36d1382ba6af8c30d41e33b28
m.bah <mohammadbahadori99@gmail.com>
2019-09-14 17:38:03 +0430

    master -delete duplicated view (v_profile_below_toolbar)

--- app/src/main/java/i/part/app/course/todo/board/ui/EditTodoListNameFragment.kt
+++ app/src/main/java/i/part/app/course/todo/board/ui/EditTodoListNameFragment.kt
@@ -22,7 +22,7 @@ class EditTodoListNameFragment : DialogFragment() {
         dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
         dialog?.setCanceledOnTouchOutside(false)
         var view = inflater.inflate(R.layout.edit_todolist_name, container, false)
-        val bt_confirm = view.findViewById<MaterialButton>(R.id.btn_edit_todolist_confirm)
+        val bt_confirm = view.findViewById(R.id.btn_edit_todolist_confirm) as MaterialButton
         val et_boardName = view.findViewById<EditText>(R.id.et_todolist_name)
         bt_confirm.setOnClickListener {
             //navigation
