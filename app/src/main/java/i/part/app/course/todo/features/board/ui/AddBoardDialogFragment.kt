fbcb57380400a25e5c05cbcabd38617c33e9e2cb
Hosein Bakand <hosein.bakand@partdp.ir>
2019-09-15 07:22:12 -0400

    setup dataBinding for add board (#53)
    add dataBinding
    move to onActivityCreated

--- app/src/main/java/i/part/app/course/todo/features/board/ui/AddBoardDialogFragment.kt
+++ app/src/main/java/i/part/app/course/todo/features/board/ui/AddBoardDialogFragment.kt
@@ -7,63 +7,69 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
-import android.widget.ImageButton
-import android.widget.ImageView
 import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.button.MaterialButton
 import com.squareup.picasso.Picasso
 import i.part.app.course.todo.R
 import i.part.app.course.todo.core.util.ui.OverlapDecoration
-import i.part.app.course.todo.core.util.ui.RoundedCornersTransformation
+import i.part.app.course.todo.databinding.FragmentAddBoardBinding
+import kotlinx.android.synthetic.main.fragment_add_board.*
 import java.util.*
-
-
-// TODO: Rename parameter arguments, choose names that match
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 
 class Add_board : DialogFragment() {
     var avatarManager: RecyclerView.LayoutManager? = null
     var avatarAdapter: RecyclerView.Adapter<*>? = null
+    lateinit var binding: FragmentAddBoardBinding
     override fun onCreateView(
-
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_board, container, false)
+        val view = binding.root
+        binding.lifecycleOwner = this
+
         dialog?.setTitle("Add BoardDetailFragment")
         dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        //dialog?.window?.setBackgroundDrawableResource(R.drawable.dialog_edge)
         dialog?.setCanceledOnTouchOutside(false)
-        val view = inflater.inflate(R.layout.fragment_add_board, container, false)
-        val closeButton = view.findViewById<ImageButton>(R.id.ib_add_board_close)
-        closeButton.setOnClickListener {
+
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // Inflate the layout for this fragment
+
+
+        ib_add_board_close.setOnClickListener {
             this.dismiss()
         }
-        val plusButton = view.findViewById<ImageButton>(R.id.ib_add_board_plus)
-        plusButton.setOnClickListener {
+        ib_add_board_plus.setOnClickListener {
             val fragmentType: String? = "add_board"
             val myBundle = bundleOf("fragmentType" to fragmentType)
             this.findNavController().navigate(R.id.action_add_board_to_addMember2, myBundle)
         }
 
-        val confirmButton = view.findViewById<MaterialButton>(R.id.btn_add_board_confirm)
-        confirmButton.setOnClickListener {
+        btn_add_board_confirm.setOnClickListener {
             this.findNavController().navigate(R.id.action_add_board_to_dashBoardFragment)
             this.dismiss()
         }
 
-        val rv_avatar = view.findViewById<RecyclerView>(R.id.rv_avatars)
+        //recycle
         val myAvatarViews: ArrayList<AvatarView> = ArrayList()
-        rv_avatar.let { it.setHasFixedSize(true) }
+
+        rv_avatars.let { it.setHasFixedSize(true) }
         val overlap: OverlapDecoration = OverlapDecoration()
-        rv_avatar.addItemDecoration(overlap)
+        rv_avatars.addItemDecoration(overlap)
         avatarManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, true)
-        rv_avatar.let { it.layoutManager = avatarManager }
+        rv_avatars.let { it.layoutManager = avatarManager }
 
         //start generating fake data
         val fakeLink: String =
@@ -77,16 +83,18 @@ class Add_board : DialogFragment() {
         //end
         val picasso = Picasso.get()
         context?.let { avatarAdapter = AvatarRecyclerAdapter(it, myAvatarViews, picasso, true) }
-        val image = view.findViewById<ImageView>(R.id.iv_add_board_preview)
-        val url =
-            "https://img.freepik.com/free-vector/colorful-watercolor-background_79603-99.jpg?size=626&ext=jpg"
-        val radius = 8
-        val margin = 0
-        val transformation = RoundedCornersTransformation(radius, margin)
-        picasso.load(url).transform(transformation)
-            .error(i.part.app.course.todo.R.drawable.person_empty)
-            .fit().into(image)
-        rv_avatar.let { it.adapter = avatarAdapter }
-        return view
+
+
+        var addBoardViewModel = AddBoardViewModel(
+            "Vahid Safari",
+            "https://img.freepik.com/free-vector/colorful-watercolor-background_79603-99.jpg?size=626&ext=jpg",
+            "vahidImage.jpg"
+        )
+        binding.newBoardDetail = addBoardViewModel
+
+
+        rv_avatars.let { it.adapter = avatarAdapter }
+
     }
+
 }
