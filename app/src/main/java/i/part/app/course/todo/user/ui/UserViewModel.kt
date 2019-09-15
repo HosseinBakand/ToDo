f979cabf992afe8b4c7ef7a984cb3ced243a9c02
vasa97 <vahid.safari@partdp.ir>
2019-09-15 10:52:57 +0430

    (#39) 9/15/2019 added end icon listener to email and phone edit text and added color change based on edit text state (enabled or disabled)

--- app/src/main/java/i/part/app/course/todo/user/ui/UserViewModel.kt
+++ app/src/main/java/i/part/app/course/todo/user/ui/UserViewModel.kt
@@ -9,9 +9,8 @@ import i.part.app.course.todo.core.util.ui.PicassoCircleTransformation
 
 
 class UserViewModel:ViewModel() {
-    val profilePhotoUrl = "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
-    var name = "Vahid Safari"
-    var email = "vahidsafari132@gmail.com"
-    var phone = "09124436345353"
-    var password = "1234567"
+    lateinit var profilePhotoUrl : String
+    lateinit var name : String
+    lateinit var email : String
+    lateinit var phone : String
 }
